45c86fa61aed6a4b20750e53f35701725603a35f
Danting Huang <serena.huangdanting@gmail.com>
2022-05-16 02:03:19 -0400

    setting up

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('client'));
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`The app is listening on port ${port}!`))
 
 
