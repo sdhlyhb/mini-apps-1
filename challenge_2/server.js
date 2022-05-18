cdaa8db69b5a80f4af37d95184214dbdfcb2ef72
Danting Huang <serena.huangdanting@gmail.com>
2022-05-18 01:52:22 -0400

    file picker features complete

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const helperFns = require('./helperFns');
+const fs = require('fs');
 
 const jsonToCsv = helperFns.jsonToCsv;
 
@@ -22,21 +25,34 @@ app.get('/', (req, res) => {
 
 
 
-
-app.post('/', (req, res)=> {
-
-
-var data = jsonToCsv(JSON.parse(req.body.inputJSON));
-console.log(data);
-res.render('index', {data:data});
-// res.send(data);
+/**********working code for input JSON in the testArea*************************/
+// app.post('/', (req, res)=> {
 
 
+// var data = jsonToCsv(JSON.parse(req.body.inputJSON));
+// console.log(data);
+// res.render('index', {data:data});
+// // res.send(data);
 
+
+// });
+
+
+app.post('/', upload.single('uploaded_file'),  function(req, res){
+  console.log('this is the file:', req.file);
+  fs.readFile(req.file.path, "utf8", function(err, jsondata) {
+    if(err) {
+      console.log('Err Reading the uploaded file!');
+    } else {
+      //console.log('this is the jsondata:', jsondata);
+      var data = jsonToCsv(JSON.parse(jsondata));
+      res.render('index', {data:data});
+
+    }
+  })
 
 
 });
-
 
 
 
