c5ae790e986d08827c4b9a33b39b3a636d26c9f0
Danting Huang <serena.huangdanting@gmail.com>
2022-05-19 03:41:40 -0400

    working on download link

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -7,13 +7,6 @@ $(document).ready(function() {
     var formData = new FormData($(this)[0]);
     console.log('this is formdata:', formData);
 
-
-  // var formData = {};
-  // var input = $('#form').serializeArray();
-  // // console.log('this is input', input);
-  // formData[input[0].name] = input[0].value;
-  // // console.log('this is formdata:', formData);
-
     $.ajax({
 
       url:'upload_json',
@@ -25,6 +18,10 @@ $(document).ready(function() {
       success: function(respose) {
         console.log('success!!!!', respose);
         $('#csvResult').text(respose);
+        $('#downloadLink').text('Download your csv report');
+        $('#downloadLink').css({'background-color': 'yellow'});
+
+
       },
       error: function(err) {
         console.log('Error submitting!!!', err);
