5122887cb3723457bc4e348bbfce42b9c4990e85
Danting Huang <serena.huangdanting@gmail.com>
2022-06-15 00:02:06 -0400

    remove extra space

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  console.log('this is the req.body:', req.body);
+
 
     res.status(200).sendFile(path.join(__dirname, '/public/index.html'));
 
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,31 @@
+const express = require('express');
+let app = express();
+const path = require('path');
 
 
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './client/dist')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/index.html'));
+});
+
+
+
+
+
+
+
+
+
+
+
+let port = 3000;
+
+app.listen(port, function () {
+  console.log(`listening on port ${port}`);
+});
